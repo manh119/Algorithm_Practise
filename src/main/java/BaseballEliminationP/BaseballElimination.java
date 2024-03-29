844177ca7546a53186e10564b3af8fe2ac396670
manh <manhnatria2@gmail.com>
2024-03-30 02:10:03 +0700

    done code + test  for baseball assigment. Math still doesn't fully understand.

--- src/main/java/BaseballEliminationP/BaseballElimination.java
+++ src/main/java/BaseballEliminationP/BaseballElimination.java
@@ -1,53 +1,195 @@
 package BaseballEliminationP;
 
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.FordFulkerson;
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballElimination {
+    private final int nTeams;
+    private final List<String> nameTeams;
+
+    private final int[] wins;
+
+    private final int[] loss;
+
+    private final int[] remain;
+
+    private final int[][] against;
+
+    private List<String> certificateOfElimination;
+
+
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
+        if (filename == null)
+            throw new IllegalArgumentException("File name should not be null");
+        In in = new In(filename);
+        nTeams = in.readInt();
+        nameTeams = new ArrayList<>();
+        wins = new int[nTeams];
+        loss = new int[nTeams];
+        remain = new int[nTeams];
+        against = new int[nTeams][nTeams];
+        readInputFile(in);
 
+    }
+
+    private void readInputFile(In in) {
+        for (int i = 0; i < nTeams; i++) {
+            String name = in.readString();
+            nameTeams.add(i, name);
+            wins[i] = in.readInt();
+            loss[i] = in.readInt();
+            remain[i] = in.readInt();
+            for (int j = 0; j < nTeams; j++) {
+                against[i][j] = in.readInt();
+            }
+        }
     }
 
     // number of teams
     public int numberOfTeams() {
-        return 0;
+        return nTeams;
     }
 
     // all teams
     public Iterable<String> teams() {
-        return null;
+        return new ArrayList<>(nameTeams);
     }
 
     // number of wins for given team
     public int wins(String team) {
-        return 0;
+        if (team == null || !nameTeams.contains(team))
+            throw new IllegalArgumentException("Invalid team parameter");
+        return this.wins[nameTeams.indexOf(team)];
     }
 
     // number of losses for given team
     public int losses(String team) {
-        return 0;
+        if (team == null || !nameTeams.contains(team))
+            throw new IllegalArgumentException("Invalid team parameter");
+        return this.loss[nameTeams.indexOf(team)];
     }
     // number of remaining games for given team
 
     public int remaining(String team) {
-        return 0;
+        if (team == null || !nameTeams.contains(team))
+            throw new IllegalArgumentException("Invalid team parameter");
+        return this.remain[nameTeams.indexOf(team)];
     }
     // number of remaining games between team1 and team2
 
     public int against(String team1, String team2) {
-        return 0;
+        if (team1 == null || !nameTeams.contains(team1) || team2 == null || !nameTeams.contains(team2))
+            throw new IllegalArgumentException("Invalid team parameter");
+        int index1 = nameTeams.indexOf(team1);
+        int index2 = nameTeams.indexOf(team2);
+        return this.against[index1][index2];
     }
+
     // is given team eliminated?
 
     public boolean isEliminated(String team) {
+        if (team == null || !nameTeams.contains(team))
+            throw new IllegalArgumentException("Invalid team parameter");
+        certificateOfElimination = new ArrayList<>();
+
+        int x = nameTeams.indexOf(team);
+
+        // trivial - case
+        for (int i = 0; i < nTeams; i++) {
+            if (i != x && wins[x] + remain[x] < wins[i]) {
+                certificateOfElimination.add(nameTeams.get(i));
+                return true;
+            }
+        }
+
+        // nontrivial - case
+        // FlowNetwork
+        int numGameVirtices = (nTeams - 1) * (nTeams - 2) / 2;
+        int t = numGameVirtices + nTeams + 1;
+        int s = t - 1;
+        FlowNetwork flowNetwork = getFlowNetwork(t, x, s);
+
+        // total game possible in = (S \ x)
+        int totalRemain = totalRemain(x);
+
+        // FordFulkerson
+        FordFulkerson fordFulkerson = new FordFulkerson(flowNetwork, s, t);
+
+        if (fordFulkerson.value() < totalRemain) {
+            // calculate certificateOfElimination
+            for (int i = 0; i < nTeams; i++) {
+                if (i != x && fordFulkerson.inCut(i)) {
+                    certificateOfElimination.add(nameTeams.get(i));
+                }
+            }
+            return true;
+        }
+
         return false;
     }
 
+    private FlowNetwork getFlowNetwork(int t, int x, int s) {
+        FlowNetwork flowNetwork = new FlowNetwork(t + 1);
+
+        // TODO : s -> game vertices
+        // TODO : game vertices -> team vertices
+        int gameVertice = nTeams;
+        for (int i = 0; i < nTeams; i++) {
+            for (int j = i + 1; j < nTeams; j++ ) {
+                if (i != x && j != x) {
+                    FlowEdge sToGameVertices = new FlowEdge(s, gameVertice, against[i][j]);
+                    FlowEdge gameVerticesToTeami = new FlowEdge(gameVertice, i, Double.POSITIVE_INFINITY);
+                    FlowEdge gameVerticesToTeamj = new FlowEdge(gameVertice, j, Double.POSITIVE_INFINITY);
+
+                    flowNetwork.addEdge(sToGameVertices);
+                    flowNetwork.addEdge(gameVerticesToTeami);
+                    flowNetwork.addEdge(gameVerticesToTeamj);
+                    gameVertice++;
+                }
+            }
+        }
+
+        // team vertices -> t
+        for (int i = 0; i < nTeams; i++) {
+            if (i != x) {
+                FlowEdge flowEdge = new FlowEdge(i, t, wins[x] + remain[x] - wins[i]);
+                flowNetwork.addEdge(flowEdge);
+            }
+        }
+        return flowNetwork;
+    }
+
+    // total game possible in Tij = (S \ x)
+    private int totalRemain(int x) {
+        int totalT = 0;
+        for (int i = 0; i < nTeams; i++) {
+            for (int j = i + 1; j < nTeams; j++) {
+                if (i != x && j != x)
+                    totalT += against[i][j];
+            }
+        }
+        return totalT;
+    }
+
+
     // subset R of teams that eliminates given team; null if not eliminated
     public Iterable<String> certificateOfElimination(String team) {
-        return null;
-
+        if (team == null || !nameTeams.contains(team))
+            throw new IllegalArgumentException("Invalid team parameter");
+        this.isEliminated(team);
+        if (certificateOfElimination.isEmpty()) return null;
+        return new ArrayList<>(certificateOfElimination);
     }
 
     public static void main(String[] args) {
-
+        BaseballElimination b = new BaseballElimination("baseball/teams5c.txt");
+        b.isEliminated("Philadelphia");
     }
+
 }
--- src/test/java/BaseballEliminationTest/BaseballEliminationTest.java
+++ src/test/java/BaseballEliminationTest/BaseballEliminationTest.java
@@ -1,4 +1,74 @@
 package BaseballEliminationTest;
 
+import BaseballEliminationP.BaseballElimination;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class BaseballEliminationTest {
+
+    @Test
+    public void testReadFileInContructor_teams4() {
+        BaseballElimination b = new BaseballElimination("baseball/teams4.txt");
+        Assertions.assertEquals(4,b.numberOfTeams());
+        Assertions.assertEquals(83,b.wins("Atlanta"));
+        Assertions.assertEquals(79,b.losses("Philadelphia"));
+        Assertions.assertEquals(3,b.remaining("Montreal"));
+        Assertions.assertEquals(1,b.against("Atlanta", "Philadelphia"));
+        Assertions.assertEquals(6,b.against("Atlanta", "New_York"));
+    }
+
+    @Test
+    public void testEliminated_teams4() {
+        BaseballElimination b = new BaseballElimination("baseball/teams4.txt");
+        Assertions.assertTrue(b.isEliminated("Philadelphia"));
+        Assertions.assertTrue(b.isEliminated("Montreal"));
+        Assertions.assertFalse(b.isEliminated("Atlanta"));
+        Assertions.assertFalse(b.isEliminated("New_York"));
+    }
+
+    @Test
+    public void testEliminated_teams4a() {
+        BaseballElimination b = new BaseballElimination("baseball/teams4a.txt");
+        Assertions.assertFalse(b.isEliminated("CIA"));
+        Assertions.assertTrue(b.isEliminated("Ghaddafi"));
+        Assertions.assertTrue(b.isEliminated("Bin_Ladin"));
+    }
+
+    @Test
+    public void testEliminated_teams5c() {
+        BaseballElimination b = new BaseballElimination("baseball/teams5c.txt");
+        Assertions.assertTrue(b.isEliminated("Philadelphia"));
+    }
+
+    @Test
+    public void testCertificateOfElimination_teams5c() {
+        BaseballElimination b = new BaseballElimination("baseball/teams5c.txt");
+        Assertions.assertTrue(b.isEliminated("Philadelphia"));
+        boolean found = false;
+        for (String team : b.certificateOfElimination("Philadelphia")) {
+            if (team.equals("Atlanta") || team.equals("Florida"))
+                found = true;
+        }
+        Assertions.assertTrue(found);
+    }
+
+    @Test
+    public void testEliminated_teams12() {
+        BaseballElimination b = new BaseballElimination("baseball/teams12.txt");
+        Assertions.assertTrue(b.isEliminated("Japan"));
+    }
+
+
+
+    @Test
+    public void testCertificateOfElimination_teams4() {
+        BaseballElimination b = new BaseballElimination("baseball/teams4.txt");
+        //Assertions.assertTrue(b.isEliminated("Philadelphia"));
+        boolean found = false;
+        for (String team : b.certificateOfElimination("Philadelphia")) {
+            if (team.equals("Atlanta") || team.equals("New_York"))
+                found = true;
+        }
+        Assertions.assertTrue(found);
+    }
 }
