e49ffc844736114e0724ba4cf07d71dc93faf71a
manh <manhnatria2@gmail.com>
2024-04-05 23:15:47 +0700

    done Percolation and test use WeightedQuickUnionUF

--- src/main/java/Algorithms_Part_1/Percolation/Percolation.java
+++ src/main/java/Algorithms_Part_1/Percolation/Percolation.java
@@ -1,39 +1,107 @@
 package Algorithms_Part_1.Percolation;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+    // nxn grid, true = is opened, false = is blocked
+    private final boolean[][] grid;
+    private final int size;
+    private int nOpen;
+    private WeightedQuickUnionUF unionUF;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-
+        if (n <= 0)
+            throw new IllegalArgumentException("n <= 0");
+        // don't use row = 0, col = 0, use from [1 -> n], size = n
+        grid = new boolean[n + 1][n + 1];
+        this.size = n + 1;
+        this.nOpen = 0;
+        // [0 -> n-1]
+        unionUF = new WeightedQuickUnionUF(size * size);
     }
+
+    private int toIndex(int row, int col) {
+        return row * this.size + col;
+    }
+
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        if (row <= 0 || row >= size || col <= 0 || col >= size)
+            throw new IllegalArgumentException("row < 0 || row > size || col < 0 || col > size");
 
+        grid[row][col] = true;
+        nOpen++;
+
+        // union with adj of a node if it is also opened
+        int minY = Math.max(col - 1, 1);
+        int maxY = Math.min(this.size - 1, col + 1);
+        int minX = Math.max(row - 1, 1);
+        int maxX = Math.min(this.size - 1, row + 1);
+
+        for (int i = minX; i <= maxX; i++) {
+            if (isOpen(i, col)) {
+                int indexIJ = toIndex(i, col);
+                int indexRowCol = toIndex(row, col);
+                unionUF.union(indexIJ, indexRowCol);
+            }
+        }
+
+        for (int i = minY; i <= maxY; i++) {
+            if (isOpen(row, i)) {
+                int indexIJ = toIndex(row, i);
+                int indexRowCol = toIndex(row, col);
+                unionUF.union(indexIJ, indexRowCol);
+            }
+        }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return false;
+        if (row <= 0 || row >= size || col <= 0 || col >= size)
+            throw new IllegalArgumentException("row < 0 || row > size || col < 0 || col > size");
+
+        return grid[row][col] == true;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
+        if (row <= 0 || row >= size || col <= 0 || col >= size)
+            throw new IllegalArgumentException("row < 0 || row > size || col < 0 || col > size");
+
+        for (int i = 1; i < this.size; i++) {
+            if (isOpen(1, i)) {
+                int indexColI = toIndex(1, i);
+                int indexRowCol = toIndex(row, col);
+                if (unionUF.find(indexColI) == unionUF.find(indexRowCol))
+                    return true;
+            }
+        }
+
         return false;
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return 0;
+        return nOpen;
     }
 
     // does the system percolate?
     public boolean percolates() {
+        for (int i = 1; i < this.size; i++) {
+            if (isOpen(this.size - 1, i) && isFull(this.size - 1, i)) {
+                return true;
+            }
+        }
+
         return false;
     }
 
     // test client (optional)
     public static void main(String[] args) {
+        WeightedQuickUnionUF unionUF1 = new WeightedQuickUnionUF(4);
+        unionUF1.union(0, 2);
 
     }
 }
--- src/test/java/Algorithms_Part_1Test/PercolationTest/PercolationTest.java
+++ src/test/java/Algorithms_Part_1Test/PercolationTest/PercolationTest.java
@@ -1,4 +1,141 @@
 package Algorithms_Part_1Test.PercolationTest;
 
+import Algorithms_Part_1.Percolation.Percolation;
+import edu.princeton.cs.algs4.In;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class PercolationTest {
+    @Test
+    public void testOpen_isOpen_input4() {
+        In in = new In("percolation/input4.txt");      // input file
+        int n = in.readInt();
+        Percolation perc = new Percolation(n);
+        while (!in.isEmpty()) {
+            int i = in.readInt();
+            int j = in.readInt();
+            perc.open(i, j);
+        }
+        // col 1 and 4 is open
+        Assertions.assertTrue(perc.isOpen(4, 1));
+        Assertions.assertTrue(perc.isOpen(3, 1));
+        Assertions.assertTrue(perc.isOpen(2, 1));
+        Assertions.assertTrue(perc.isOpen(1, 1));
+        Assertions.assertTrue(perc.isOpen(1, 4));
+        Assertions.assertTrue(perc.isOpen(2, 4));
+        Assertions.assertTrue(perc.isOpen(3, 4));
+        Assertions.assertTrue(perc.isOpen(4, 4));
+        // col 2 and col 3 is not open
+        Assertions.assertFalse(perc.isOpen(4, 2));
+        Assertions.assertFalse(perc.isOpen(3, 2));
+        Assertions.assertFalse(perc.isOpen(2, 2));
+        Assertions.assertFalse(perc.isOpen(1, 2));
+        Assertions.assertFalse(perc.isOpen(4, 3));
+        Assertions.assertFalse(perc.isOpen(3, 3));
+        Assertions.assertFalse(perc.isOpen(2, 3));
+        Assertions.assertFalse(perc.isOpen(1, 3));
+        // col 0 and row 0 is not use and thus throw exception
+        Assertions.assertThrows(IllegalArgumentException.class, () -> perc.isOpen(0, 1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> perc.isOpen(0, 2));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> perc.isOpen(0, 3));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> perc.isOpen(0, 4));
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> perc.isOpen(1, 0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> perc.isOpen(2, 0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> perc.isOpen(3, 0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> perc.isOpen(4, 0));
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> perc.isOpen(0, 0));
+
+
+    }
+
+    @Test
+    public void test_isFull_input4() {
+        In in = new In("percolation/input4.txt");      // input file
+        int n = in.readInt();
+        Percolation perc = new Percolation(n);
+        while (!in.isEmpty()) {
+            int i = in.readInt();
+            int j = in.readInt();
+            perc.open(i, j);
+        }
+        // col 1 and 4 is full
+        Assertions.assertTrue(perc.isFull(4, 1));
+        Assertions.assertTrue(perc.isFull(3, 1));
+        Assertions.assertTrue(perc.isFull(2, 1));
+        Assertions.assertTrue(perc.isFull(1, 1));
+        Assertions.assertTrue(perc.isFull(1, 4));
+        Assertions.assertTrue(perc.isFull(2, 4));
+        Assertions.assertTrue(perc.isFull(3, 4));
+        Assertions.assertTrue(perc.isFull(4, 4));
+
+        // col 2 and col 3 is not full
+
+        Assertions.assertFalse(perc.isFull(4, 2));
+        Assertions.assertFalse(perc.isFull(3, 2));
+        Assertions.assertFalse(perc.isFull(2, 2));
+        Assertions.assertFalse(perc.isFull(1, 2));
+        Assertions.assertFalse(perc.isFull(4, 3));
+        Assertions.assertFalse(perc.isFull(3, 3));
+        Assertions.assertFalse(perc.isFull(2, 3));
+        Assertions.assertFalse(perc.isFull(1, 3));
+    }
+
+    @Test
+    public void testPercolation_input4() {
+        In in = new In("percolation/input4.txt");      // input file
+        int n = in.readInt();
+        Percolation perc = new Percolation(n);
+        while (!in.isEmpty()) {
+            int i = in.readInt();
+            int j = in.readInt();
+            perc.open(i, j);
+        }
+        Assertions.assertTrue(perc.percolates());
+    }
+
+    @Test
+    public void testPercolation_input8_no() {
+        In in = new In("percolation/input8-no.txt");      // input file
+        int n = in.readInt();
+        Percolation perc = new Percolation(n);
+        while (!in.isEmpty()) {
+            int i = in.readInt();
+            int j = in.readInt();
+            perc.open(i, j);
+        }
+        Assertions.assertFalse(perc.percolates());
+    }
+
+    @Test
+    public void testPercolation_input2_no() {
+        In in = new In("percolation/input2-no.txt");      // input file
+        int n = in.readInt();
+        Percolation perc = new Percolation(n);
+        while (!in.isEmpty()) {
+            int i = in.readInt();
+            int j = in.readInt();
+            perc.open(i, j);
+        }
+        Assertions.assertFalse(perc.percolates());
+    }
+
+    @Test
+    public void testPercolation_input10() {
+        In in = new In("percolation/input10.txt");      // input file
+        int n = in.readInt();
+        Percolation perc = new Percolation(n);
+        while (!in.isEmpty()) {
+            int i = in.readInt();
+            int j = in.readInt();
+            perc.open(i, j);
+        }
+        Assertions.assertFalse(perc.isFull(n ,n - 1));
+        Assertions.assertTrue(perc.isFull(n - 1,n));
+        Assertions.assertTrue(perc.isFull(n,n));
+        Assertions.assertTrue(perc.percolates());
+    }
+
+
 }
