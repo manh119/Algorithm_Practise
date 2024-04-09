b1e9ff44f651b709a573e643274baae72a8922c2
manh <manhnatria2@gmail.com>
2024-04-09 16:04:32 +0700

    use priority queue + implement comparable from class, make class comparable

--- src/main/java/org/example/Sort/filterRestaurants.java
+++ src/main/java/org/example/Sort/filterRestaurants.java
@@ -2,32 +2,47 @@ package org.example.Sort;
 
 import edu.princeton.cs.algs4.In;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
 public class filterRestaurants {
+    class Pair implements Comparable<Pair> {
+        int id;
+        int rate;
+        Pair(int id, int rate) {
+            this.id = id;
+            this.rate = rate;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            if (this.rate == o.rate)
+                return o.id - this.id;
+            else
+                return o.rate - this.rate;
+        }
+    }
     public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
-        return Arrays
-                .stream(restaurants)
-                .filter(ints -> ints[2] >= veganFriendly && ints[3] <= maxPrice && ints[4] <= maxDistance)
-                .sorted((res1, res2) -> res1[1] == res2[1] ? res2[0] - res1[0] : res2[1] - res1[1])
-                .map(ints -> ints[0])
-                .collect(Collectors.toList());
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for(int[] res : restaurants) {
+            if (res[2] >= veganFriendly && res[3] <= maxPrice && res[4] <= maxDistance)
+                pq.add(new Pair(res[0], res[1]));
+        }
+        List<Integer> ans= new ArrayList<>();
+        while(!pq.isEmpty()) {
+            ans.add(pq.poll().id);
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
         int[][] restaurants = {{1, 4, 1, 40, 10}, {2, 8, 0, 50, 5}, {3, 8, 1, 30, 4}, {4, 10, 0, 10, 3}, {5, 1, 1, 15, 1}};
         int veganFriendly = 0, maxPrice = 50, maxDistance = 10;
-
-        List<Integer> filtered = Arrays
-                .stream(restaurants)
-                .filter(ints -> ints[2] >= veganFriendly && ints[3] <= maxPrice && ints[4] <= maxDistance)
-                .sorted((res1, res2) -> res1[1] == res2[1] ? res2[0] - res1[0] : res2[1] - res1[1])
-                .map(ints -> ints[0])
-                .collect(Collectors.toList());
-        System.out.println(filtered);
-
+        filterRestaurants filterRestaurants = new filterRestaurants();
+        System.out.println(filterRestaurants.filterRestaurants(restaurants, veganFriendly, maxPrice, maxDistance));
 
     }
 }
